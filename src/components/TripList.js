172ae2f53b4f8c00b2838a51403f0cc0237b03ea
Alfaiz Mac <macalangcomalfaiz2@gmail.com>
2025-01-21 20:23:15 +0800

    FINISHED!

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,29 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./TripList.css";
+import { useFetch } from "../hook/useFetch";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
 
-  useEffect(() => {
-    // Fetch the trips data when the component mounts
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setTrips(json))
-      .catch((error) => console.error("Error fetching trips:", error)); // Catch errors
-  }, [url]); // Empty dependency array ensures this runs once when the component mounts
+  //this can get the data of the data base the algorithm is on useFetch.js
+  const { data: trips, isPending, error } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
+      {isPending && <div>Loading trips....</div>}
+      {error && <div>{error}</div>}
       <ul>
-        {/* Loop through the trips and display them */}
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trips) => (
+            <li key={trips.id}>
+              <h3>{trips.title}</h3>
+              <p>{trips.price}</p>
+            </li>
+          ))}
       </ul>
       <div className="filter">
         <button onClick={() => setUrl("http://localhost:3000/trips?loc=mall")}>
---
+++ src/hook/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        //if there is a error then this only execute the bottom will not
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("The fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Coould not fetch the data");
+        }
+      }
+    };
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
